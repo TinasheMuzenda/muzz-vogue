0775b9a2fafba99760274c31570cf822ee9030d2
TinasheMuzenda <tmuzendat@gmail.com>
2025-11-28 12:39:42 +0200

    fixed some bugs and socket

--- server/src/index.jsx
+++ server/src/index.jsx
@@ -9,37 +9,38 @@ const PORT = process.env.PORT || 7000;
 
 const server = http.createServer(app);
 
-export const createSocketServer = (server) => {
-  const io = new Server(server, {
-    cors: {
-      origin: process.env.CLIENT_URL || "http://localhost:5173",
-      credentials: true,
-      methods: ["GET", "POST"],
-    },
-  });
+// export const createSocketServer = (server) => {
+//   const io = new Server(server, {
+//     cors: {
+//       origin: process.env.CLIENT_URL || "http://localhost:5173",
+//       credentials: true,
+//       methods: ["GET", "POST"],
+//     },
+//   });
 
-  io.use(socketAuth);
+//   io.use(socketAuth);
 
-  io.on("connection", (socket) => {
-    const user = socket.user;
+//   io.on("connection", (socket) => {
+//     const user = socket.user;
 
-    if (user?.isAdmin) {
-      socket.join("admins");
-    }
+//     if (user?.isAdmin) {
+//       socket.join("admins");
+//     }
 
-    socket.on("message-from-client", (msg) => {
-      if (user?.isAdmin) {
-        io.to("admins").emit("admin-message", msg);
-      }
-    });
+//     socket.on("message-from-client", (msg) => {
+//       if (user?.isAdmin) {
+//         io.to("admins").emit("admin-message", msg);
+//       }
+//     });
 
-    socket.on("disconnect", () => {});
-  });
+//     socket.on("disconnect", () => {});
+//   });
 
-  return io;
-};
+//   return io;
+// };
 
-export const io = createSocketServer(server);
+// Call the imported function
+createSocketServer(server);
 
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
